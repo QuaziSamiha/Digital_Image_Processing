337b1f4805f3453703c952ce9d954e5ed705556f
Quazi Samiha Tasnim <1703155@student.ruet.ac.bd>
2022-08-31 08:48:45 +0600

    reading image and convert rgb to gray image

--- practice.py
+++ practice.py
@@ -1,1 +1,17 @@
-print('samiha')
+import cv2
+import numpy as np
+
+# how to read an image
+
+img = cv2.imread("images/dog.jpg")
+
+print("Image Type : " , type(img))
+print("Shape : " , img.shape)
+
+cv2.imshow("window", img)
+cv2.waitKey(0)
+
+
+img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+cv2.imshow("window", img_gray)
+cv2.waitKey(0)
---
+++ rgb_to_grayScale.py
@@ -0,0 +1,9 @@
+import imp
+import cv2
+import numpy as np
+
+img = cv2.imread('images/dog.jpg')
+
+img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+cv2.imshow("window", img_gray)
+cv2.waitKey(0)
