d00fcf24d32c81e200b710eccfbdfeeb35f6cb10
Quazi Samiha Tasnim <1703155@student.ruet.ac.bd>
2022-09-01 08:50:57 +0600

    rgb to grayscale convert

--- rgb_to_grayScale.py
+++ rgb_to_grayScale.py
@@ -7,3 +7,5 @@ img = cv2.imread('images/dog.jpg')
 img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 cv2.imshow("window", img_gray)
 cv2.waitKey(0)
+
+print("Gray Scale Image Shape: ", img_gray.shape)
